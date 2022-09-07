d9e6c7d74ddb4d3e184bca125db08f33f2623557
esadakman <esadd26@gmail.com>
2022-09-07 15:00:08 +0300

    styling

--- main/settings.py
+++ main/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'crispy_forms',
     # my Apps
     'todo',
 ]
@@ -122,6 +123,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
